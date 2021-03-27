00db89895297a93bf4e7dcb2ceefaff1c910ec71
Muhammad Tayyab <66192491+Muhammad-Tayyab1@users.noreply.github.com>
2021-03-27 09:30:31 +0500

    add

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,15 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
-
+import React, { useState } from 'react';
+import { Button, FlatList, Image, ScrollView, SectionList, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Components } from './component';
+import logo from './assets/logo.png'; 
 export default function App() {
+  const [text, setText] = useState('');
   return (
+    <ScrollView>
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>First App</Text>
       <Text>Hello Tayyab</Text>
-      <Button title="clickME"></Button>
+      <Button onPress={()=> alert('Button Clicked')} title="clickME"></Button>
+      <Components />
+      <Image source={logo} style={{ width: 305, height: 159 }} /> 
+      <Image source={logo} style={{ width: 305, height: 159 }} /> 
+      <Image source={logo} style={{ width: 305, height: 159 }} /> 
       <StatusBar style="auto" />
-    </View>
+        <TextInput
+        style={{height: 40}}
+        placeholder="Type here to translate!"
+        onChangeText={text => setText(text)}
+        defaultValue={text}
+      />
+      <Text style={{padding: 10, fontSize: 42}}>
+        {text.split(' ').map((word) => word && '🍕').join(' ')}
+      </Text>
+      <FlatList
+        data={[
+          {key: 'Devin'},
+          {key: 'Dan'},
+          {key: 'Dominic'},
+          {key: 'Jackson'},
+          {key: 'James'},
+          {key: 'Joel'},
+          {key: 'John'},
+          {key: 'Jillian'},
+          {key: 'Jimmy'},
+          {key: 'Julie'},
+        ]}
+        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+      />
+      <SectionList
+          sections={[
+            {title: 'D', data: ['Devin', 'Dan', 'Dominic']},
+            {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+          renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+          keyExtractor={(item, index) => index}/>
+    </View></ScrollView>
   );
 }
 
@@ -19,5 +58,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
   },
 });
---
+++ my-app/component.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from 'react-native'
+
+export const Components = () => {
+    return (
+       
+            <Button onPress={()=> alert('Component Button Clicked')} title="Click ME"></Button>
+        
+    )
+}
